aeff182ad6afa948b6b4175d35e386371f512471
渡辺太一 <watanabetaichi@watanabetaichi-no-MacBook-Air-2.local>
2015-09-07 10:19:07 +0900

    PHPサンプル2-3-8

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -23,9 +23,9 @@ class SampleController < ApplicationController
 
   def test6
       @text1 = params[:text1]
-      p '======='
-      p @text1
-      p '======='
+  end
+  def test7
+      @text1,@text2,@text3,@text4 = params[:text1],params[:text2],params[:text3],params[:text4]
   end
 
   def aa(count,id)
