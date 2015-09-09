e16322f6933767cfe42cd69e8abdb86ff6f60636
渡辺太一 <watanabetaichi@watanabetaichi-no-MacBook-Air-2.local>
2015-09-09 10:19:33 +0900

    PHPサンプル2-3-9

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -27,6 +27,16 @@ class SampleController < ApplicationController
   def test7
       @text1,@text2,@text3,@text4 = params[:text1],params[:text2],params[:text3],params[:text4]
   end
+  def test8
+    @form = params[:text1]
+    if request.post? then
+      if @form.blank? then
+        @test1 = "入力してください"
+      else
+        @test1 = params[:text1]
+      end
+    end
+  end
 
   def aa(count,id)
   	
